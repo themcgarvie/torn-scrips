4467db9de02b8a2397e7a96ab16e96e79eea8937
themcgarvie <bmm@mcgarvie.net>
2026-01-12 09:35:35 +0000

    Test Script

---
+++ test/test.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2026-01-11
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("Testing");
+})();
